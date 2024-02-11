5f40ac651f0799d0bc5ee22d117b2bc9c46da0a8
EduardCRadd <105921610+EduardCRadd@users.noreply.github.com>
2024-02-11 19:54:55 +0000

    UI adjustments

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,7 @@ module.exports = {
   bracketSpacing: true,
   bracketSameLine: true,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   printWidth: 120,
   tabWidth: 2,
-};
+}
