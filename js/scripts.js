0d917b6266564cc7c258dd785e219406b91bd844
EduardCRadd <105921610+EduardCRadd@users.noreply.github.com>
2024-02-11 19:41:11 +0000

    Build UI and optimisation for desktop, tablet, smartphone

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// window.onload = function () {
+//   let header = document.querySelector('.sectionIntro__header')
+//   let outline = document.querySelector('.sectionIntro__outline')
+//   let spacing = 20 // Desired spacing in pixels
+//
+//   outline.style.top = header.offsetHeight + spacing + 'px'
+// }
+// document.addEventListener('DOMContentLoaded', function () {
+//   const menuText = document.querySelector('.nav__text')
+//   const diamondSVG = document.querySelector('object[data="./assets/icons/diamond.svg"]')
+//   const burgerMenuSVG = document.querySelector('object[data="./assets/icons/burger-menu.svg"]')
+//
+//   menuText.addEventListener('click', function () {
+//     console.log('Menu text clicked')
+//   })
+//
+//   diamondSVG.addEventListener('click', function () {
+//     console.log('Diamond SVG clicked')
+//   })
+//
+//   burgerMenuSVG.addEventListener('click', function () {
+//     console.log('Burger menu SVG clicked')
+//   })
+// })
+
+/////////////////
